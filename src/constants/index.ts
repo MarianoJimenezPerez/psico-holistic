dfe60727febd14db5bd78d68ff9dbffa0995e315
mariano jimenez <marianojimenezperez1@gmail.com>
2024-10-23 11:05:09 +0200

    feat: links change

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,27 +5,19 @@ export const WHATSAPP_URI = "https://wa.me/34635067158?text=";
 export const SERVICES: Service[] = [
   {
     id: "sesion1:1",
-    name: "Sesión 1:1",
-    link: WHATSAPP_URI + "Hola! Me gustaría reservar cita para sesión 1:1",
+    name: "Aplica aquí a una Sesión de Diagnóstico",
+    link: "https://docs.google.com/forms/d/e/1FAIpQLSfIzZPi0hwfAJoQTH7fTg_kzJgo8z5Ed-UVFmH8VA11twBziQ/viewform",
   },
   {
     id: "aperturaregistrosakashicos",
-    name: "Apertura de Registros Akáshicos",
-    link:
-      WHATSAPP_URI +
-      "Hola! Me gustaría reservar cita para Apertura de Registros Akáshicos",
+    name: `Aplica aquí a mi Acompañamiento Individual "Relaciones Libres y Conscientes"`,
+    link: "https://docs.google.com/forms/d/e/1FAIpQLSdffeBFNMqH0QUa6PQWaUplzCOgIxibFeWN07cfbSbIN_yTrw/viewform",
   },
   {
     id: "tapping",
-    name: "Tapping",
-    link:
-      WHATSAPP_URI + "Hola! Me gustaría reservar cita para audio de Tapping",
-  },
-  {
-    id: "full",
-    name: "Tapping + Escritura Terapéutica + Sesión 1:1",
+    name: "No sabes cuál es la mejor opción para tu momento? Contáctame al Whatsapp",
     link:
       WHATSAPP_URI +
-      "Hola! Me gustaría reservar cita para Tapping + Escritura Terapéutica + Sesión 1:1",
+      "Hola, Aldana! Me gustaría tener más información sobre tus sesiones de psicoterapia. Gracias!",
   },
 ];
